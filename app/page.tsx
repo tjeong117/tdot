c6e71bda19f327a3d326c84e3bd8d1d82608f00e
Tom <tomjeong0324@gmail.com>
2026-03-30 13:26:44 -0700

    dd

--- app/page.tsx
+++ app/page.tsx
@@ -16,21 +16,6 @@ export default function Page() {
         >
           Datafruit
         </a>
-        {`.`}
-      </p>
-      <p className="mb-4">
-        {` I'm  a recent grad from Georgia Institute of Technology where I studied Computer Science and Mathematics.`}
-      </p>
-      <p className="mb-4">
-        {`I'm currently the cofounder and CTO of `}
-        <a
-          href="https://datafruit.ai"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline"
-        >
-          Datafruit
-        </a>
         {` (`}
         <a
           href="https://www.ycombinator.com/companies/datafruit"
@@ -43,10 +28,22 @@ export default function Page() {
         {`) where we are building AI systems for enterprise software implementation teams.`}
       </p>
       <p className="mb-4">
-        {`Currnelty building QuDDPM`}
+        {`I'm a recent grad from Georgia Institute of Technology where I studied Computer Science and Mathematics.`}
       </p>
       <p className="mb-4">
-        {`You can reach out to me via LinkedIn`}
+        {`Currently building QuDDPM — a quantum denoising diffusion model for information scrambling.`}
+      </p>
+      <p className="mb-4">
+        {`You can reach out to me via `}
+        <a
+          href="https://www.linkedin.com/in/tomjeong"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
+          LinkedIn
+        </a>
+        {`.`}
       </p>
       <div className="my-8">
         <BlogPosts />
